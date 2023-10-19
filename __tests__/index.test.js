231a2c5d65d6c27d03520c6d68b2fa0f386cfb57
rjglozano <ryan.rjl38@gmail.com>
2023-10-19 19:04:14 +0800

    jest

---
+++ __tests__/index.test.js
@@ -0,0 +1,27 @@
+const { add, subtract, multiply, divide } = require("../index");
+// import {add, subtract, multiply, divide} from "../index";
+
+
+it("should add two numbers correctly", ()=>{
+   expect(add(1, 1)).toBe(2);})
+
+   
+it("should add two numbers correctly", ()=>{
+    expect(subtract(1, 1)).toBe(2);})
+
+       
+it("should add two numbers correctly", ()=>{
+    expect(multiply(4, 6)).toBe(2);})
+
+it("should add two numbers correctly", ()=>{
+    expect(divide(10, 5)).toBe(2);})
+
+       
+
+
+    
+ 
+
+
+ 
+
---
+++ index.js
@@ -0,0 +1,23 @@
+const add = (a, b)=>{
+    return a + b;
+};
+
+const subtract = (a, b)=>{
+    return a - b;
+};
+
+const multiply = (a, b)=>{
+    return a * b;
+};
+
+const divide = (a, b)=>{
+    return a / b;
+};
+
+
+module.exports = {
+    add,
+    subtract,
+    multiply,
+    divide
+  };
