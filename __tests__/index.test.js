df42e07c0b7fd9fd16feadeb579c053294ef276e
rjglozano <ryan.rjl38@gmail.com>
2023-10-19 19:32:50 +0800

    new examples

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,20 +1,39 @@
-const { add, subtract, multiply, divide } = require("../index");
-// import {add, subtract, multiply, divide} from "../index";
+const { add, subtract, multiply, divide, factorial, palindrome } = require("../index");
 
 
 it("should add two numbers correctly", ()=>{
    expect(add(1, 1)).toBe(2);})
 
    
-it("should add two numbers correctly", ()=>{
-    expect(subtract(1, 1)).toBe(2);})
+it("should subtract two numbers correctly", ()=>{
+    expect(subtract(1, 1)).toBe(0);})
 
        
-it("should add two numbers correctly", ()=>{
-    expect(multiply(4, 6)).toBe(2);})
+it("should multiple two numbers correctly", ()=>{
+    expect(multiply(4, 6)).toBe(24);})
 
-it("should add two numbers correctly", ()=>{
+it("should divide two numbers correctly", ()=>{
     expect(divide(10, 5)).toBe(2);})
+
+ 
+describe("Should do factorial", () => {
+    it("should factorial one number correctly", ()=>{
+        expect(() => factorial(-2)).toThrow("Factorial is not defined for negative number");
+    })
+
+    it("should return 1 of the n factproal is zero", ()=>{
+        expect(factorial(0)).toBe(1);
+    })
+
+    it("should return the factorial of n that is positive", ()=>{
+        expect(factorial(4)).toBe(24);
+    })
+})
+
+it("should palindrome string correctly", ()=>{
+expect(palindrome("wow")).toBe(true);})
+    
+
 
        
 
--- index.js
+++ index.js
@@ -14,10 +14,33 @@ const divide = (a, b)=>{
     return a / b;
 };
 
+function factorial(n){
+    if(n < 0){
+        throw new Error ("Factorial is not defined for negative number")
+    }
+    
+    //base case
+    if(n == 0 || n == 1){
+        return 1;
+    //recursive case
+    }else{
+        return n * factorial(n-1);
+    }
+}
+
+function palindrome(str) {
+    var re = /[\W_]/g;
+    var lowRegStr = str.toLowerCase().replace(re, '');
+    var reverseStr = lowRegStr.split('').reverse().join(''); 
+    return reverseStr === lowRegStr;
+  }
+
 
 module.exports = {
     add,
     subtract,
     multiply,
-    divide
+    divide,
+    factorial,
+    palindrome
   };
